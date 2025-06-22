382676ed868aab6e8084aa5c830a9cd24ccf9aa1
EpharGy <ephargy@gmail.com>
2025-06-22 16:36:59 +0930

    v1.1.8 - 🛠 Fixed Build process

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 Version information for Traktor DJ NowPlaying Discord Bot
 """
 
-__version__ = "1.1.7"
-__version_info__ = (1, 1, 7)
+__version__ = "1.1.8"
+__version_info__ = (1, 1, 8)
 
 # For detailed changelog and release history, see:
 # - GitHub Releases: https://github.com/EpharGy/Traktor-DJ-NowPlaying-Discord-Bot/releases
