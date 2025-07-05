02a79f6eeea29b0f3f64eaa37b009a1e61da8681
EpharGy <ephargy@gmail.com>
2025-07-05 16:44:38 +0930

    v1.6.6 - Minor UI fixes

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 Version information for TraCord DJ
 """
 
-__version__ = "1.6.5"
-__version_info__ = (1, 6, 5)
+__version__ = "1.6.6"
+__version_info__ = (1, 6, 6)
 
 # For detailed changelog and release history, see:
 # - GitHub Releases: https://github.com/EpharGy/TraCord-DJ/releases
