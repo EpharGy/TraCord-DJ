5c4ceea41d0955668501b986508a211dda24bda6
EpharGy <ephargy@gmail.com>
2025-07-08 19:26:13 +0930

    v1.7.4 - Overlay UI implemented

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 Version information for TraCord DJ
 """
 
-__version__ = "1.7.2"
-__version_info__ = (1, 7, 2)
+__version__ = "1.7.4"
+__version_info__ = (1, 7, 4)
 
 # For detailed changelog and release history, see:
 # - GitHub Releases: https://github.com/EpharGy/TraCord-DJ/releases
